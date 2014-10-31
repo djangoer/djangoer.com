30fdd43e70b995ade0e66e19f91457070c2fb19c
suhailvs <suhailvs@gmail.com>
2014-10-31 02:46:15 +0000

    added Django-mama-cas

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^cas/', include('mama_cas.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
